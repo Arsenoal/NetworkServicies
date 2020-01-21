bb5bdaf5c46d87ed13cb4aac4f6209602aad720e
Arsen Budumyan <arsenbudumyan@ArsenBudumyanA1707.local>
2020-01-21 15:29:37 +0400

    service method signature update

--- networkproperties/src/main/java/com/example/networkproperties/servicie/sample/SampleService.kt
+++ networkproperties/src/main/java/com/example/networkproperties/servicie/sample/SampleService.kt
@@ -1,7 +1,9 @@
 package com.example.networkproperties.servicie.sample
 
+import retrofit2.http.GET
 import retrofit2.http.Url
 
 interface SampleService {
+    @GET
     suspend fun getData(@Url url: String): SampleResponse
 }
