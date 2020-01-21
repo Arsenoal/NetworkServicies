3f478ee24ed3b66700a2469f55022dc7f4d41ef5
Arsen Budumyan <arsenbudumyan@ArsenBudumyanA1707.local>
2020-01-21 16:31:22 +0400

    current commit

--- networkproperties/src/main/java/com/example/networkproperties/servicie/sample/SampleResponse.kt
+++ networkproperties/src/main/java/com/example/networkproperties/servicie/sample/SampleResponse.kt
@@ -3,5 +3,7 @@ package com.example.networkproperties.servicie.sample
 import com.google.gson.annotations.SerializedName
 
 data class SampleResponse(
-    @SerializedName("response") val response: String
+    @SerializedName("response") val response: Response
 )
+
+data class Response(@SerializedName("numFound") val numFound: Int)
